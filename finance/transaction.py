608fe75dfad8b59f71bad36228b4485ee324f097
musaCODEzz <musamaxwell15@gmail.com>
2025-06-04 21:34:19 +0300

    Refactored the entire Transaction.py code

--- finance/transaction.py
+++ finance/transaction.py
@@ -1,29 +1,21 @@
-# from datetime import datetime
-
-# class Transaction:
-#     def __init__(self, amount: float, category: str, description: str = "", date: str = None):
-#         if amount <= 0:
-#             raise ValueError("Amount must be positive")
-#         self.amount = amount
-#         self.category = category
-#         self.description = description
-#         self.date = datetime.strptime(date, "%Y-%m-%d") if date else datetime.now()
-
-#     def __repr__(self):
-#         return f"{self.date.date()} | {self.category} | {self.amount:.2f} | {self.description}"
-
+from datetime import datetime
 
 class Transaction:
-    def __init__(self, amount, description, category):
-        amount = float(amount)
+    def __init__(self, amount: float , category: str, description: str = "", date: str = None):
         if amount <= 0:
             raise ValueError("Amount must be positive")
         self.amount = amount
-        self.description = description
         self.category = category
+        self.description = description
+        self.date = date if date else datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-amount = input("Enter transaction amount: ")
-description = input("Enter transaction description: ").strip().title()
+
+    def __repr__(self):
+        return f"{self.date} | {self.description} | {self.category} | {self.amount:.2f}"
+
+amount = float(input("Enter transaction amount: "))
 category = input("Enter transaction category (income/expense): ").strip().lower()
-transaction = Transaction(amount, description, category)
-print(transaction.__dict__)
+description = input("Enter transaction description: ").strip().title()
+
+transaction = Transaction(amount, category, description)
+print(transaction)
